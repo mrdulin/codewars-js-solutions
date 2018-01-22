961823488cb8403d940ad90221d3f8a386a79b3c
mrdulin <novaline@qq.com>
2018-01-22 22:55:44 +0800

    完善抽奖-指哪打哪

--- examples/跑马灯+抽奖/index.js
+++ examples/跑马灯+抽奖/index.js
@@ -5,25 +5,37 @@ window.onload = function () {
   const $ul = $('.light-list');
   const $stopBtn = $('#stop');
   const $startBtn = $('#start');
-  const defaultActiveIndex = 0;
+  const $form = $('#form');
+
+  const defaultActiveIndex = 4;
   const duration = 200;
   const nodes = [];
   const activeClass = 'active';
-  const lightCount = 10;
-  const acceleratedSpeed = -100;
+  const lightCount = 9;
   const plusSpeedStep = 20;
   const maxSpeed = 40;
   const minSpeed = 1000 + maxSpeed;
   const congratulation = '恭喜你，中了500万软妹币';
   const off = 'off', on = 'on';
-
+  const defaultLuckNumber = -1;
+  let acceleratedSpeed = -100;
+  let luckNumber = defaultLuckNumber;
   let activeIndex = defaultActiveIndex;
   let lightSwitch = off;
+  let leftNum = 0;
 
   $stopBtn.addEventListener('click', onStopBtnClick, false);
-  $startBtn.addEventListener('click', onStartBtnClick, false);
+  $form.addEventListener('submit', onFormSubmit, false);
 
   renderLights();
+
+  function onFormSubmit(e) {
+    e.preventDefault();
+    const input = this.elements[0];
+    luckNumber = Number.parseInt(input.value.trim()) || luckNumber;
+    console.log('luckNumber: ', luckNumber);
+    onStartBtnClick();
+  }
 
   function onStopBtnClick() {
     lightSwitch = off;
@@ -65,18 +77,34 @@ window.onload = function () {
   //抽奖
   function loop(speed) {
     const timer = setTimeout(function () {
-      if (speed < minSpeed) {
+      if (speed < minSpeed || leftNum) {
         processLightCssClass();
       }
 
       if (lightSwitch === off) {
-        speed -= acceleratedSpeed;
+        // console.log('activeIndex lightSwitch === off: ', activeIndex);
+        if (!leftNum) {
+          speed -= acceleratedSpeed;
+        }
         if (speed <= minSpeed) {
+          if (speed - acceleratedSpeed > minSpeed) {
+            if (activeIndex > luckNumber) {
+              leftNum = lightCount - Math.abs(activeIndex - luckNumber);
+            } else if (activeIndex <= luckNumber) {
+              leftNum = luckNumber - activeIndex;
+            }
+          }
           loop(speed);
         } else {
-          clearTimeout(timer);
-          console.log(congratulation)
-          alert(congratulation);
+          if (leftNum) {
+            leftNum--;
+            // console.log('leftNum: ', leftNum, ' speed: ', speed);
+            loop(speed);
+          } else {
+            clearTimeout(timer);
+            console.log(congratulation)
+            alert(congratulation);
+          }
         }
       } else {
         if (speed > maxSpeed) {
@@ -93,7 +121,7 @@ window.onload = function () {
 
     activeIndex++;
     if (activeIndex > (lightCount - 1)) {
-      activeIndex = defaultActiveIndex;
+      activeIndex = 0;
     }
     const nextNode = nodes[activeIndex];
     nextNode.classList.add(activeClass);
